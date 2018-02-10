5e8e4093d6f8a9e97786c2ec577ef3468ce6a24c
CherishClark <cherish.g.clark@gmail.com>
2018-02-10 11:15:19 -0700

    creating helper to determine if a student's first name is unique and if it is not unique, initial of last name will be appended to first name else firstname is returned.

--- app/helpers/student_helper.rb
+++ app/helpers/student_helper.rb
@@ -1,3 +1,18 @@
 module StudentHelper
 
+
+  def unique_firstname(student)
+    allStudentFirstNames = []
+
+    Student.all.each do |student|
+      allStudentFirstNames << student.firstname
+    end   
+
+    if allStudentFirstNames.count(student.firstname) > 1
+      return student.firstname + " " + student.lastname[0]+ "."
+    else
+      return student.firstname
+    end
+  end
+
 end
