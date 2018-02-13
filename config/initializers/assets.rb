408b3efb77f79aabe2e16901a8a8b4ff13779853
CherishClark <cherish.g.clark@gmail.com>
2018-02-13 14:47:35 -0700

    correcting the order of javascript files to precompile correctly

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile +=   %w( angular-front-end/inline.bundle.js angular-front-end/styles.bundle.js angular-front-end/main.bundle.js)
+Rails.application.config.assets.precompile +=   %w( angular-front-end/inline.bundle.js
+  angular-front-end/polyfills.bundle.js
+  angular-front-end/styles.bundle.js
+  angular-front-end/vendor.bundle.js
+  angular-front-end/main.bundle.js)
